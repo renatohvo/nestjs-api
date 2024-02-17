ade4542f8230d83f3a3472d8c5871565f9cb95f2
renatohvo <renatohvo@gmail.com>
2024-02-17 15:17:42 -0300

    typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './products/entities/product.entity';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'root',
+      password: 'root',
+      database: 'nest',
+      entities: [Product],
+      synchronize: true,
+      logging: true
+    }),
+    ProductsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
