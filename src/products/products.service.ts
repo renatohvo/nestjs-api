082e13db025eb50f4720de0cbd4c6e7d247df5d8
renatohvo <renatohvo@gmail.com>
2024-02-20 23:12:25 -0300

    product find, create and fixture

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class ProductsService {
+
+  constructor(@InjectRepository(Product) private productRepo: Repository<Product>){}
+
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    const product = this.productRepo.create(createProductDto);
+    return this.productRepo.save(product);
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.productRepo.find();
   }
 
   findOne(id: number) {
